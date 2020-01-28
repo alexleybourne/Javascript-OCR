df561ba71ac459bc1de7c4ebea4b1993f56478aa
alexleybourne <alexleybourne1@gmail.com>
2020-01-29 08:29:39 +1000

    Imports and upload folder created

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hey")
+const express = require('express')
+const app = express()
+const fs = require('fs')
+const multer = require('multer')
+const {TesseractWorker} = require('tesseract.js')
+const worker = new TesseractWorker()
+
+const storage = multer.diskStorage({
+    destination: (req,res,cb) => {
+        cb(null, "./uploads")
+    }
+})
