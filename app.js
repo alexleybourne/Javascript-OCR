92e378ac0b5323965d177ebf2c7e0dc9cb1a6d59
alexleybourne <alexleybourne1@gmail.com>
2020-01-30 09:00:01 +1000

    Successfully deletes uploaded and created files after use

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require('express')
 const app = express()
 const fs = require('fs')
 const multer = require('multer')
-
 const {createWorker} = require('tesseract.js')
-
 
 // Storage
 const storage = multer.diskStorage({
@@ -17,7 +15,6 @@ const storage = multer.diskStorage({
 })
 
 const upload = multer({storage: storage}).single('avatar')
-
 app.set('view engine', 'ejs')
 
 // Routes
@@ -25,19 +22,24 @@ app.get('/', (req,res) => {
     res.render('index')
 })
 
+newFile = ""
+createdFile = "None"
+
 app.post('/upload', (req,res) => {
     upload(req,res, err => {
         console.log(req.file)
         const path = `./uploads/${req.file.originalname}`
+        lastFile = ''
         fs.readFile(path, (err, data) => {
             if(err) return console.log("Error Occured")
-            
+
             const image = path
             console.log(`Recognizing ${req.file.originalname}`)
 
-            // // Removes original Images .png, .jpeg etc.
-            // let saveName = req.file.originalname.toString()
-            // saveName = (saveName.split('.')[0])
+            // Removes original Images file extension .png, .jpeg etc.
+            let saveName = req.file.originalname.toString()
+            saveName = (saveName.split('.')[0])
+
 
             // For some reason it has to be like this or it crashes??
             OCR()
@@ -55,20 +57,44 @@ app.post('/upload', (req,res) => {
             console.log(text)
             const { data } = await worker.getPDF('Tesseract OCR Result')
 
-            fs.writeFileSync(`ReadBot.pdf`, Buffer.from(data))
-            console.log(`Generate PDF: ReadBot.pdf`)
+            // If user uploads same image twice
+            if (createdFile === `${__dirname}/created/${saveName}-ReadBot.pdf`) {
+                newFile = `${__dirname}/created/${saveName}V2-ReadBot.pdf`
+            } else {
+                newFile = `${__dirname}/created/${saveName}-ReadBot.pdf`
+            }
+
+            fs.writeFileSync(newFile, Buffer.from(data))
+            console.log(`Generate PDF: ${saveName}-ReadBot.pdf`)
 
             res.redirect('/download')
 
+            console.log(createdFile)
+            
+
             await worker.terminate()
             })()}
+
+            app.get('/download', (req, res) => {
+
+                file = newFile
+            
+                res.download(file)
+
+                // Deletes Uploaded Image
+                fs.unlinkSync(image)
+
+                
+                if (createdFile !== "None") {
+                    fs.unlinkSync(createdFile)
+                }
+                
+
+                createdFile = file
+            })
+
         })
     })
-})
-
-app.get('/download', (req, res) => {
-    const file = `${__dirname}/ReadBot.pdf`
-    res.download(file)
 })
 
 
