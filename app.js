1c2bd0ce840758a756bd29e2a57a0171f904e009
alexleybourne <alexleybourne1@gmail.com>
2020-01-30 00:07:32 +1000

    Routes and Uploads now Working

--- app.js
+++ app.js
@@ -2,19 +2,35 @@ const express = require('express')
 const app = express()
 const fs = require('fs')
 const multer = require('multer')
-const {TesseractWorker} = require('tesseract.js')
-const worker = new TesseractWorker()
+const {createWorker} = require('tesseract.js')
+const worker = createWorker()
 
+// Storage
 const storage = multer.diskStorage({
-    destination: (req,res,cb) => {
+    destination: (req,file,cb) => {
         cb(null, "./uploads")
     },
-    filename: (req,res,cb) => {
-        cb(null, req.file)
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
     }
-
 })
 
 const upload = multer({storage: storage}).single('avatar')
 
 app.set('view engine', 'ejs')
+
+// Routes
+app.get('/', (req,res) => {
+    res.render('index')
+})
+
+app.post('/upload', (req,res) => {
+    upload(req,res, err => {
+        console.log(req.file)
+    })
+})
+
+
+// Start up server
+const PORT = 5000 || process.env.PORT
+app.listen(PORT, () => console.log(`Hey I'm running on port ${PORT}`))
