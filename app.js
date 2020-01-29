ad57563d9f8bfcd6a5edc21c238bf3c163360e95
alexleybourne <alexleybourne1@gmail.com>
2020-01-30 06:58:19 +1000

    File Downloads

--- app.js
+++ app.js
@@ -35,9 +35,9 @@ app.post('/upload', (req,res) => {
             const image = path
             console.log(`Recognizing ${req.file.originalname}`)
 
-            // Removes original Images .png, .jpeg etc.
-            let saveName = req.file.originalname.toString()
-            saveName = (saveName.split('.')[0])
+            // // Removes original Images .png, .jpeg etc.
+            // let saveName = req.file.originalname.toString()
+            // saveName = (saveName.split('.')[0])
 
             // For some reason it has to be like this or it crashes??
             OCR()
@@ -53,17 +53,23 @@ app.post('/upload', (req,res) => {
             await worker.initialize('eng')
             const { data: { text } } = await worker.recognize(image)
             console.log(text)
-            res.send(text)
             const { data } = await worker.getPDF('Tesseract OCR Result')
 
-            fs.writeFileSync(`${saveName}-Converted.pdf`, Buffer.from(data))
-            console.log(`Generate PDF: ${saveName}-Converted.pdf`)
+            fs.writeFileSync(`ReadBot.pdf`, Buffer.from(data))
+            console.log(`Generate PDF: ReadBot.pdf`)
+
+            res.redirect('/download')
+
             await worker.terminate()
             })()}
         })
     })
 })
 
+app.get('/download', (req, res) => {
+    const file = `${__dirname}/ReadBot.pdf`
+    res.download(file)
+})
 
 
 // Start up server
