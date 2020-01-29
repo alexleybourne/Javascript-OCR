b292010736da9b2cd62b134dce0d9733ef9c390e
alexleybourne <alexleybourne1@gmail.com>
2020-01-29 23:37:08 +1000

    View Created

--- app.js
+++ app.js
@@ -8,5 +8,13 @@ const worker = new TesseractWorker()
 const storage = multer.diskStorage({
     destination: (req,res,cb) => {
         cb(null, "./uploads")
+    },
+    filename: (req,res,cb) => {
+        cb(null, req.file)
     }
+
 })
+
+const upload = multer({storage: storage}).single('avatar')
+
+app.set('view engine', 'ejs')
